97a3e796eb075086abf9ac4bf586200788a4fbd1
Ian Cahn <iancahn@live.com>
2022-07-02 14:29:06 +0200

    Content Update
    Added the Popular Games and NewGames sections.
    
    Added more styling and GlobalStyles.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import Home from './pages/Home';
+// import styles
+import GlobalStyles from './components/GlobalStyles';
 
 
 function App() {
 
   return (
     <div className="App">
+      <GlobalStyles />
       <Home />
     </div>
   );
