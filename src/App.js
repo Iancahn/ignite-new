870dbe1ea5fa5a07a55c3aee97e24e6db5f7e0f3
Ian Cahn <iancahn@live.com>
2022-07-07 18:56:45 +0200

    Updating Nav and Styling
    Added some basic CSS to the NAV component.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
+// import Components and Pages
 import Home from './pages/Home';
+import Nav from './components/Nav';
 // import styles
 import GlobalStyles from './components/GlobalStyles';
 import { Routes, Route, useLocation } from 'react-router-dom';
@@ -9,6 +11,7 @@ function App() {
   return (
     <div className="App">
       <GlobalStyles />
+      <Nav />
       <Routes>
         {/* <Route path={["game/:id", "/"]}> */}
         <Route path="/" element={<Home />}></Route>
